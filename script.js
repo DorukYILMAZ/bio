6f85274496886e7d9c4c50f16e7035ea59ba0982
Doruk Yılmaz <114815196+DorukYILMAZ@users.noreply.github.com>
2022-10-01 16:16:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Öğrenciyim", "Web Developerım", "Tasarımcıyım", "Freelancerım","Anxlize"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
